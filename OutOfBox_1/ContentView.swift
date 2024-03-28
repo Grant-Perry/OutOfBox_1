c591008abbdad6957eee186fdfbd100a9ea64dfd
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2024-03-28 12:34:34 -0500

    Initial Commit

---
+++ OutOfBox_1/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   OutOfBox_1
+//
+//   Created by: Grant Perry on 3/28/24 at 12:34 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OutOfBox_1/OutOfBox_1App.swift
@@ -0,0 +1,19 @@
+//   OutOfBox_1App.swift
+//   OutOfBox_1
+//
+//   Created by: Grant Perry on 3/28/24 at 12:34 PM
+//     Modified: 
+//
+//  Copyright © 2024 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct OutOfBox_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
